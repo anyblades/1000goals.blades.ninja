51ba5d6166ae0533a6c5de53e8e70fd10e4c1877
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-05-20 16:23:46 -0600

    refactor: remove package.json and update netlify build base directory to .11ty

--- .11ty/eleventy.config.js
+++ .11ty/eleventy.config.js
@@ -5,13 +5,13 @@ import pkg from "./package.json" with { type: "json" };
 export default function (eleventyConfig) {
   baseConfig(eleventyConfig);
 
-  eleventyConfig.setIncludesDirectory("./.subtle/_includes/");
-  eleventyConfig.addPassthroughCopy({ "./.subtle/_public/": "." });
+  eleventyConfig.setIncludesDirectory("./.11ty/_includes/");
+  eleventyConfig.addPassthroughCopy({ "./.11ty/_public/": "." });
   eleventyConfig.addPassthroughCopy("../**/*.png");
   eleventyConfig.addGlobalData("site", pkg.site);
 
   //TODO: move to 11ty-blades?
   for (const line of readFileSync("./.gitignore", "utf8").split("\n"))
     if (line && !line.startsWith("#"))
-      eleventyConfig.watchIgnores.add(`../.subtle/${line}`);
+      eleventyConfig.watchIgnores.add(`../.11ty/${line}`);
 }
